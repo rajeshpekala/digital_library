67d77dee27891bb75b722d1d06b656a078df4c91
rajeshpekala <pekalarajesh001@gmail.com>
2024-01-28 19:42:54 +0530

    something

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className=' h-screen bg-gradient-to-b from-black to-gray-800
     p-4 text-white'>
-        <h1 className='text-white text-2xl'>A digital library is an online collection of resources, such as books, articles, and multimedia, 
+        <h1 className='text-white font-bold text-3xl'>A digital library is an online collection of resources, such as books, articles, and multimedia, 
             accessible through digital means. It provides users with the ability to access, search, and retrieve information electronically. Digital libraries often aim to preserve and provide easy, 
             remote access to a wide range of materials, fostering efficient knowledge dissemination.</h1>
     </div>
